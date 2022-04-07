99ce9607f502b362a496e9454c01e6272407bca7
Safoine Moncef Amine <>
2022-04-07 23:24:30 +0000

    Show first screen + reorder files

--- challenge/challenge/classes/AppDelegate.swift
+++ challenge/challenge/classes/AppDelegate.swift
@@ -1,20 +1,14 @@
-//
-//  AppDelegate.swift
-//  challenge
-//
-//  Created by Safoine Moncef Amine on 7/4/2022.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 }
